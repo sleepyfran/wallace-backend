cd0208c386192d30a2048317d2abc0a6a766ef80
Fran González <fgonzaleva@icloud.com>
2020-04-05 22:43:13 +0200

    :fire: Starting from scratch with .NET Core

--- lib/wallace/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule Wallace.Repo do
-  use Ecto.Repo,
-    otp_app: :wallace,
-    adapter: Ecto.Adapters.Postgres
-end
--- lib/wallace/schemas/types/repetition.ex
+++
@@ -1,3 +0,0 @@
-defmodule Wallace.Schemas.Types.Repetition do
-  use Exnumerator, values: [:never, :daily, :weekly, :monthly, :biannually, :annually]
-end
